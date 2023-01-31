0990bd4ff78212a177df40be93ffed981dad6376
AislingLC <123462760+AislingLC@users.noreply.github.com>
2023-01-31 10:17:49 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(name='systeminfo2', \
+    version='1.0', \
+    description='this package contains some cpu benchmarking code', \
+    author='Aisling Cooke', \
+    url='https://github.com/AislingLC/systeminfo2', \
+    packages= find_packages()
+)
